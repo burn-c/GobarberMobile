dd740e0c673a9e3bf5cd229c9f32a2676572d819
burn-c <chopcsom@gmail.com>
2019-12-31 22:37:19 -0300

    Config: InitialRoute

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -36,7 +36,7 @@ export default function SignIn({ navigation }) {
         <Form>
           <FormInput
             icon="mail-outline"
-            keyboardType="email-adress"
+            keyboardType="email-address"
             autoCorrent={false}
             autoCapitalize="none"
             placeholder="Digite seu e-mail"
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,24 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
+// Routes Sign
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
-export default createAppContainer(createSwitchNavigator({ SignIn, SignUp }));
+// Routes App
+import Dashboard from './pages/Dashboard';
+
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({ SignIn, SignUp }),
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
