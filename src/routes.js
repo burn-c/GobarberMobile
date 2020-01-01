2400bfe1e2c3a492474b68c5cf692173a086680b
burn-c <chopcsom@gmail.com>
2019-12-31 22:55:12 -0300

    Styles: Routes

--- src/routes.js
+++ src/routes.js
@@ -7,15 +7,29 @@ import SignUp from './pages/SignUp';
 
 // Routes App
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 export default (isSigned = false) =>
   createAppContainer(
     createSwitchNavigator(
       {
         Sign: createSwitchNavigator({ SignIn, SignUp }),
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255,255,255,0.7)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          }
+        ),
       },
       {
         initialRouteName: isSigned ? 'App' : 'Sign',
